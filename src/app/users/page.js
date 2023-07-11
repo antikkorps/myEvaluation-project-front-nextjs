9d37990846c8a31be7b5fb3975ebbb40f7ace3f8
Franck <franckvienot7@gmail.com>
2023-07-12 00:26:21 +0200

    added basic table and data fetching for users from the backend

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,20 +1,40 @@
-import axios from 'axios';
+async function getData() {
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+  const res = await fetch(process.env.NEXT_PUBLIC_API_URL + '/users');
 
-function page() {
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(
-        process.env.NEXT_PUBLIC_API_URL + '/users'
-      );
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
 
-  fetchData();
-
-  return <div>This is the users index</div>;
+  return res.json();
 }
 
-export default page;
+export default async function Page() {
+  const data = await getData();
+  console.log(data);
+  return (
+    <main>
+      <div>This is the users index</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>First Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.firstName}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
+}
