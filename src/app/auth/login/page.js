8345cc33a689fc728246f80d4a0b9afc051a6527
Franck <franckvienot7@gmail.com>
2023-07-14 23:15:56 +0200

    started configuring nextauth

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -1,10 +1,14 @@
-function Login() {
+export default function Login() {
   return (
     <div>
       <h1>This is the login page</h1>
       <p>Log Me in Please</p>
+      <h2>Login</h2>
+      <form>
+        <input type='email' placeholder='Email' />
+        <input type='password' placeholder='Password' />
+        <input type='submit' value='Login' />
+      </form>
     </div>
   );
 }
-
-export default Login;
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,9 +1,10 @@
-function page() {
+function Dashboard() {
   return (
     <div>
       <h1>Dashboard page</h1>
+      <p>This is the main page when you are connected</p>
     </div>
   );
 }
 
-export default page;
+export default Dashboard;
