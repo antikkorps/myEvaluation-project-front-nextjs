2e984848e58329df2a2d2bb4efc7c161e8b0cd06
Franck <franckvienot7@gmail.com>
2023-07-18 23:25:30 +0200

    made some changes in profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,10 +1,20 @@
-const Profile = () => {
+import { useSession } from 'next-auth/react';
+
+export default function Profile() {
+  const { data: session, status } = useSession();
+
+  if (status === 'loading') {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
       <h1>Your Profile Page</h1>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
+      {session ? (
+        <pre>{JSON.stringify(session, null, 2)}</pre>
+      ) : (
+        <p>You are not logged in</p>
+      )}
     </>
   );
-};
-
-export default Profile;
+}
