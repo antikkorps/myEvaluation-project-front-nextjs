d9c9346edba07a89e4cbcde80fbce3904baacc8e
Franck <franckvienot7@gmail.com>
2023-07-19 22:39:24 +0200

    commented on page.js

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,20 +1,20 @@
 import { useSession } from 'next-auth/react';
 
 export default function Profile() {
-  const { data: session, status } = useSession();
+  // const { data: session, status } = useSession();
 
-  if (status === 'loading') {
-    return <p>Loading...</p>;
-  }
+  // if (status === 'loading') {
+  //   return <p>Loading...</p>;
+  // }
 
   return (
     <>
       <h1>Your Profile Page</h1>
-      {session ? (
+      {/* {session ? (
         <pre>{JSON.stringify(session, null, 2)}</pre>
       ) : (
         <p>You are not logged in</p>
-      )}
+      )} */}
     </>
   );
 }
